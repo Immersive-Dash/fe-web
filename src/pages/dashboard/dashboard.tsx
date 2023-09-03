b04cec2150b35848a8a88a0e51bb5cb2b7669e57
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-09-03 13:00:47 +0700

    feat : add dashboard page

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,7 +1,22 @@
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="">
+      <div className="grid grid-cols-3 gap-5 mx-5 py-5">
+        <div className="h-44 flex justify-end px-3 py-3 bg-slate-200">
+          Mentee Actiove
+        </div>
+        <div className="h-44 flex justify-end px-3 py-3 bg-slate-200">
+          Mentee Placement
+        </div>
+        <div className="h-44 flex justify-end px-3 py-3 bg-slate-200">
+          Mentee Feedback
+        </div>
+      </div>
+      <div className="grid grid-cols-1 mx-5">
+        <div className="bg-slate-200 h-72">Chart</div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
