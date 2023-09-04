0ea141d533d1cb2e773bf4796ac8c67adeb2c345
Geofannyher <geofannyher@gmail.com>
2023-09-04 22:36:36 +0700

    fix: ui new log

--- src/pages/mentee/detailFeedPage.tsx
+++ src/pages/mentee/detailFeedPage.tsx
@@ -12,35 +12,40 @@ const DetailFeedPage = () => {
     return (
         <div className="px-8 py-10 container mx-auto">
             <div className="grid grid-cols-12 items-center px-4">
+                <div className="col-span-12 py-2 mb-5 ">
+                    <h1 className='font-semibold text-2xl'>
+                        Feedback
+                    </h1>
+                </div>
                 <div className="col-span-6">
                     <div className="flex flex-col">
                         <div>
                             <h1 className="text-3xl font-bold">Asep Udin</h1>
                         </div>
                         <div>
-                            <p className="text-lg font-semibold">Frontend Batch 15</p>
+                            <p className="text-xl">Frontend Batch 15</p>
                         </div>
                         <div>
-                            <h1 className=" text-sm">TI</h1>
+                            <h1 className="text-xl  ">TI</h1>
                         </div>
                         <div>
-                            <h1 className="text-lg font-semibold">Universitas Brawijaya</h1>
+                            <h1 className="text-xl">Universitas Brawijaya</h1>
                         </div>
                     </div>
                 </div>
                 <div className="col-span-6 flex justify-end">
                     <div className="flex flex-col gap-2">
                         <div>
-                            <h1 className="text-sm">phone : 085729830543 </h1>
+                            <h1 className="text-xl font-semibold">phone : <span className='font-normal'> 085729830543</span>  </h1>
                         </div>
                         <div>
-                            <p className="text-sm">telegram : @udinasep</p>
+                            <p className="text-xl font-semibold">telegram : <span className='font-normal'>  @udinasep</span></p>
                         </div>
                         <div>
-                            <p className="text-sm">discord : @udinasep#123</p>
+                            <p className="text-xl font-semibold">discord : <span className='font-normal'> @udinasep#123</span> </p>
                         </div>
                         <div>
-                            <p className="text-sm">email :udinasep@gmail.com</p>
+                            <p className="text-xl font-semibold">email : <span className='font-normal'> udinasep@gmail.com</span></p>
                         </div>
                     </div>
                 </div>
@@ -76,7 +81,7 @@ const DetailFeedPage = () => {
                                         </div>
                                         <div className="flex gap-2 py-2 justify-end">
                                             <button type="submit" onClick={() => handleClose()} className=" text-white bg-[#E05252] focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center">Cancel</button>
-                                            <button type="submit" className=" text-white bg-[#03034F] focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center">Add</button>
+                                            <button type="submit" className=" text-white bg-[#3E31DF] focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center">Add</button>
                                         </div>
                                     </form>
                                 </div>
@@ -87,7 +92,7 @@ const DetailFeedPage = () => {
             }
             <div className="space-y-3">
                 <div className="border-2 flex gap-2 rounded-md border-black p-2">
-                    <div className="text-sm w-1/4 font-semibold">
+                    <div className="text-lg w-1/4 font-semibold">
                         <h1>
                             End of Section
                         </h1>
@@ -99,14 +104,14 @@ const DetailFeedPage = () => {
                         </p>
                     </div>
                     <div className="gap-y-4 flex flex-col">
-                        <p className="text-sm">
+                        <p className="text-lg">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                             ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
                             anim id est laborum.
                         </p>
-                        <div className="font-semibold">
+                        <div className="font-semibold text-lg">
                             <p>
                                 Change Status: Continue Section
                             </p>
