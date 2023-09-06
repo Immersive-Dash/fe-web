c4c9fc1670702b73774fca98272d6297f489a7d9
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-09-06 17:09:56 +0700

    feat:show list data user

--- src/pages/user/userPage.tsx
+++ src/pages/user/userPage.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { LuEdit, LuTrash, LuXCircle } from 'react-icons/lu';
 import Popup from '../../components/popup/popup';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import { toast } from 'react-hot-toast';
+
+interface Data {
+  full_name: string;
+  team: string;
+  email: string;
+}
 const UserPage = () => {
   const [open, setOpen] = useState(false);
   const [openEdit, setOpenEdit] = useState(false);
-  const [fullName, setFullname] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [role, setrole] = useState('')
-
+  const [userData, setUserData] = useState<Data[]>([]);
+  const [fullName, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setrole] = useState('');
 
   const handleClose = () => {
     setOpen(false);
@@ -24,22 +31,50 @@ const UserPage = () => {
   const handleOpenEdit = () => {
     setOpenEdit(true);
   };
-  const handleAdd = () => {
-    axios.post(`https://62c3aad4876c4700f540123e.mockapi.io/users`, {
-      full_name: fullName,
-      email: email,
-      password: password,
-      role: role,
-    }).then((response) => {
-      console.log(response.data)
-      setOpen(false)
-      toast.success('Data Akun User Berhasil Ditambahkan')
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
-  return (
 
+  const getUser = async () => {
+    try {
+      const accountData = Cookies.get('account');
+      const account = JSON.parse(accountData);
+      const storedToken = account.token;
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/users`,
+        {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        }
+      );
+      console.log(response.data.data);
+      setUserData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const handleAdd = () => {
+    axios
+      .post(`https://62c3aad4876c4700f540123e.mockapi.io/users`, {
+        full_name: fullName,
+        email: email,
+        password: password,
+        role: role,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setOpen(false);
+        toast.success('Data Akun User Berhasil Ditambahkan');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
     <div className="p-10">
       <div className="flex p-4 justify-between items-center">
         <h1 className="font-semibold text-2xl">List User</h1>
@@ -76,7 +111,7 @@ const UserPage = () => {
                       <input
                         type="text"
                         name="email"
-                        onChange={((e) => setFullname(e.target.value))}
+                        onChange={(e) => setFullname(e.target.value)}
                         className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
                       />
                     </div>
@@ -87,7 +122,7 @@ const UserPage = () => {
                       <input
                         type="text"
                         name="email"
-                        onChange={((e) => setEmail(e.target.value))}
+                        onChange={(e) => setEmail(e.target.value)}
                         className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
                       />
                     </div>
@@ -99,7 +134,7 @@ const UserPage = () => {
                     <input
                       type="text"
                       name="password"
-                      onChange={((e) => setPassword(e.target.value))}
+                      onChange={(e) => setPassword(e.target.value)}
                       className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
                     />
                   </div>
@@ -107,7 +142,10 @@ const UserPage = () => {
                     <label className="block text-sm font-semibold text-black">
                       Role
                     </label>
-                    <select onChange={(e) => setrole(e.target.value)} className="py-2 px-2 w-full rounded bg-slate-200">
+                    <select
+                      onChange={(e) => setrole(e.target.value)}
+                      className="py-2 px-2 w-full rounded bg-slate-200"
+                    >
                       <option>Academic</option>
                       <option>People Skills</option>
                       <option>Placement</option>
@@ -163,119 +201,123 @@ const UserPage = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b">
-              <th scope="row" className="px-6 py-4">
-                1
-              </th>
-              <td className="px-6 py-4">Gatot</td>
-              <td className="px-6 py-4">gatot@gmail.com</td>
-              <td className="px-6 py-4">Academic</td>
-              <td className="px-6 py-4">Admin</td>
-              <td className="px-6 py-4">Active</td>
-              <td className="px-6 py-4 flex gap-2">
-                <div className="cursor-pointer">
-                  <LuTrash size={20} />
-                </div>
-                <div
-                  onClick={() => handleOpenEdit()}
-                  className="cursor-pointer"
-                >
-                  <LuEdit size={20} />
-                </div>
-                {openEdit && (
-                  <Popup onConfirm={handleCloseEdit}>
-                    <div className="relative w-full max-w-md max-h-full">
-                      <div className="relative bg-white rounded-lg shadow">
-                        <button
-                          type="button"
-                          onClick={() => handleCloseEdit()}
-                          className="absolute top-3 right-2.5 bg-transparent hover:bg-gray-200 rounded-full text-black w-8 h-8 inline-flex justify-center items-center"
-                          data-modal-hide="authentication-modal"
-                        >
-                          <LuXCircle size={20} />
-                        </button>
-                        <div className="px-6 py-6 lg:px-8">
-                          <h3 className="mb-4 text-2xl font-semibold text-black">
-                            Edit User
-                          </h3>
-                          <form className="space-y-4" action="#">
-                            <div className="flex gap-3">
-                              <div>
-                                <label className="block text-sm font-semibold text-black">
-                                  Full Name
-                                </label>
-                                <input
-                                  type="text"
-                                  name="email"
-                                  className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
-                                />
-                              </div>
-                              <div>
-                                <label className="block text-sm font-semibold text-black">
-                                  Email
-                                </label>
-                                <input
-                                  type="text"
-                                  name="email"
-                                  className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
-                                />
-                              </div>
-                            </div>
-                            <div>
-                              <label className="block text-sm font-semibold text-black">
-                                Team
-                              </label>
-                              <input
-                                type="text"
-                                name="email"
-                                className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
-                              />
-                            </div>
-                            <div className="grid grid-cols-2 gap-3">
-                              <div>
-                                <label className="block text-sm font-semibold text-black">
-                                  Role
-                                </label>
-                                <select className="py-2 px-2 w-full rounded bg-slate-200">
-                                  <option>Academic</option>
-                                  <option>People Skills</option>
-                                  <option>Placement</option>
-                                </select>
-                              </div>
-                              <div>
-                                <label className="block text-sm font-semibold text-black">
-                                  Status
-                                </label>
-                                <select className="py-2 px-2 w-full rounded bg-slate-200">
-                                  <option>Active</option>
-                                  <option>Not-Active</option>
-                                </select>
-                              </div>
-                            </div>
-
-                            <div className="flex gap-2 py-2 justify-end">
-                              <button
-                                type="submit"
-                                onClick={() => handleCloseEdit()}
-                                className=" text-white bg-[#E05252] focus:ring-4 focus:outline-none font-semibold rounded-full text-sm px-10 py-2 text-center"
-                              >
-                                Cancel
-                              </button>
-                              <button
-                                type="submit"
-                                className=" text-white bg-[#3E31DF] focus:ring-4 focus:outline-none font-semibold rounded-full text-sm px-10 py-2 text-center"
-                              >
-                                Edit
-                              </button>
-                            </div>
-                          </form>
-                        </div>
-                      </div>
+            {userData.map((item, index) => {
+              return (
+                <tr key={index} className="bg-white border-b">
+                  <th scope="row" className="px-6 py-4">
+                    {index + 1}
+                  </th>
+                  <td className="px-6 py-4">{item.full_name}</td>
+                  <td className="px-6 py-4">{item.email}</td>
+                  <td className="px-6 py-4">{item.team}</td>
+                  <td className="px-6 py-4">Admin</td>
+                  <td className="px-6 py-4">Active</td>
+                  <td className="px-6 py-4 flex gap-2">
+                    <div className="cursor-pointer">
+                      <LuTrash size={20} />
                     </div>
-                  </Popup>
-                )}
-              </td>
-            </tr>
+                    <div
+                      onClick={() => handleOpenEdit()}
+                      className="cursor-pointer"
+                    >
+                      <LuEdit size={20} />
+                    </div>
+                    {openEdit && (
+                      <Popup onConfirm={handleCloseEdit}>
+                        <div className="relative w-full max-w-md max-h-full">
+                          <div className="relative bg-white rounded-lg shadow">
+                            <button
+                              type="button"
+                              onClick={() => handleCloseEdit()}
+                              className="absolute top-3 right-2.5 bg-transparent hover:bg-gray-200 rounded-full text-black w-8 h-8 inline-flex justify-center items-center"
+                              data-modal-hide="authentication-modal"
+                            >
+                              <LuXCircle size={20} />
+                            </button>
+                            <div className="px-6 py-6 lg:px-8">
+                              <h3 className="mb-4 text-2xl font-semibold text-black">
+                                Edit User
+                              </h3>
+                              <form className="space-y-4" action="#">
+                                <div className="flex gap-3">
+                                  <div>
+                                    <label className="block text-sm font-semibold text-black">
+                                      Full Name
+                                    </label>
+                                    <input
+                                      type="text"
+                                      name="email"
+                                      className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
+                                    />
+                                  </div>
+                                  <div>
+                                    <label className="block text-sm font-semibold text-black">
+                                      Email
+                                    </label>
+                                    <input
+                                      type="text"
+                                      name="email"
+                                      className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
+                                    />
+                                  </div>
+                                </div>
+                                <div>
+                                  <label className="block text-sm font-semibold text-black">
+                                    Team
+                                  </label>
+                                  <input
+                                    type="text"
+                                    name="email"
+                                    className=" border border-gray-300 text-black text-sm rounded-sm  block w-full p-2.5"
+                                  />
+                                </div>
+                                <div className="grid grid-cols-2 gap-3">
+                                  <div>
+                                    <label className="block text-sm font-semibold text-black">
+                                      Role
+                                    </label>
+                                    <select className="py-2 px-2 w-full rounded bg-slate-200">
+                                      <option>Academic</option>
+                                      <option>People Skills</option>
+                                      <option>Placement</option>
+                                    </select>
+                                  </div>
+                                  <div>
+                                    <label className="block text-sm font-semibold text-black">
+                                      Status
+                                    </label>
+                                    <select className="py-2 px-2 w-full rounded bg-slate-200">
+                                      <option>Active</option>
+                                      <option>Not-Active</option>
+                                    </select>
+                                  </div>
+                                </div>
+
+                                <div className="flex gap-2 py-2 justify-end">
+                                  <button
+                                    type="submit"
+                                    onClick={() => handleCloseEdit()}
+                                    className=" text-white bg-[#E05252] focus:ring-4 focus:outline-none font-semibold rounded-full text-sm px-10 py-2 text-center"
+                                  >
+                                    Cancel
+                                  </button>
+                                  <button
+                                    type="submit"
+                                    className=" text-white bg-[#3E31DF] focus:ring-4 focus:outline-none font-semibold rounded-full text-sm px-10 py-2 text-center"
+                                  >
+                                    Edit
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </Popup>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
